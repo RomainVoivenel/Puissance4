45de0c3659bc57a251890a87170d67391df2221b
Kyxtaka <randriantsoanathan1@gmail.com>
2024-06-12 11:01:07 +0200

    modif exec mais bug present

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -5,21 +5,27 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Puissance4MenuVSJoueurs extends Application {
+public class Puissance4 extends Application {
+
     @Override 
     public void init() throws Exception {
         
     }
     
     @Override 
-    public void start(Stage primarystage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Puissance4.fxml"));
+    public void start(Stage primarystage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("file:resources/fxml/puissance4.fxml"));
         BorderPane root = loader.load();
         Scene mainScene = new Scene(root);
 
         primarystage.setScene(mainScene);
         primarystage.setTitle("test");
         
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("test");
     }
     
     
